7ce0607c58212eb82b73e3595a56983e16ef8246
Vega <tianlas@outlook.com>
2026-01-16 21:58:23 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://47.109.103.4:21114".to_owned()
 }
 
 #[inline]
